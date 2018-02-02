c1ccaf35ecdee16726fab0e627040f581d865412
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-02 15:12:03 +0100

    ol

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// We moeten er eerst voor zorgen dat de hele html pagina geladen is in de browser
+$(document).ready(function () {
+  // Hier komt vanaf nu al je jquery code!
+
+  $("ol").css("border", "5px solid orange");
+  $("ol").css("padding", "25px");
+  $("ol").css("width", "400px");
+
+
+  
+});
+
+
