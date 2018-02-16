06aa7f6d9a272f0c4b7c46bcffa89978a2272f2f
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-16 15:31:20 +0100

    after

--- manipulating content/js/script.js
+++ manipulating content/js/script.js
@@ -8,6 +8,18 @@ $(document).ready(function () {
   $("[class*=alert]").on("click", function () {
     $("#left li:last").prepend("Dit is wat tekst voor de oorspronkelijke tekst. ");
   });
+
+  $("[class*=danger]").on("click", function () {
+    var newListItem = $("<li>Dit is een heel nieuw listitem na de derde li</li>");
+    newListItem.css({"background-color" : "orange"});
+    $("#left li:eq(2)").after(newListItem);
+  });
+
+  $("[class*=success]").on("click", function () {
+    var newListItem = $("<li>Dit is een heel nieuw listitem voor de eerste li</li>");
+    newListItem.css({"background-color" : "orange"});
+    $("#left li:first").before(newListItem);
+  });
 });
 
 
