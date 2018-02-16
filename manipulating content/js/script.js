9cc5279d8b4e8ff0db1df3042402238d7f003c9c
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-16 15:00:07 +0100

    Nieuwe tekst ervoor

--- manipulating content/js/script.js
+++ manipulating content/js/script.js
@@ -1,7 +1,13 @@
 // We moeten er eerst voor zorgen dat de hele html pagina geladen is in de browser
 $(document).ready(function () {
   // Hier komt vanaf nu al je jquery code!
-    
+  $("[class*=primary]").on("click", function () {
+    $("#left li:first").append(". Dit is wat tekst na de oorspronkelijke tekst");
+  });
+  
+  $("[class*=alert]").on("click", function () {
+    $("#left li:last").prepend("Dit is wat tekst voor de oorspronkelijke tekst. ");
+  });
 });
 
 
