c7f5ac379a5b241dbf265587a4e8cf92295ead3a
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-03-28 15:30:22 +0200

    ajax

--- jquery-ajax/js/script.js
+++ jquery-ajax/js/script.js
@@ -9,8 +9,15 @@ $(document).ready(function () {
     $.post("http://localhost/2017-2018/blok3/k-frte-am1b/jquery-ajax/data.php", 
            {"category" :  value}, 
            function (data) {
-              console.log(data);
-              $(".jumbotron p").eq(1).text(data);
+              // console.log(data);
+              //$(".jumbotron p").eq(1).text(data);
+              for ( var i=0; i<data.length; i++) {
+                //console.log(data[i].info);
+                $(".carousel-inner img").eq(i).attr("src", "./images/" + data[i].name);
+                $(".carousel-inner h5").eq(i).text(data[i].title);
+                $(".carousel-inner p").eq(i).text(data[i].info);
+
+              }
            },
            "json");
 
